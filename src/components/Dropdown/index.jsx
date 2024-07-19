6ba15b83d9c10529458cbdb2e0bbafca0e771790
Carlo Santos <rcarlosantos89@gmail.com>
2024-07-18 20:16:12 -0500

    first iteration of dropdown animation

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -4,7 +4,7 @@ import "./Dropdown.scss";
 const Dropdown = ({ dropdownVisible }) => {
   return (
     <>
-      <section className={`dropdown ${dropdownVisible ? "" : "hidden"}`}>
+      <section className={`dropdown ${dropdownVisible ? "visible" : ""}`}>
         <span className="dropdown--link">Home</span>
         <span className="dropdown--link">Philosophy</span>
         <span className="dropdown--link">Provider</span>

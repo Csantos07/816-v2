0279920e15e4d2cee2f2d3b132550be92b33e0a8
Carlo Santos <rcarlosantos89@gmail.com>
2024-07-26 18:11:47 -0500

    working mobile responsiveness kind of desktop

--- src/components/Main/HeroSection/index.jsx
+++ src/components/Main/HeroSection/index.jsx
@@ -4,7 +4,10 @@ import "./HeroSection.scss";
 const HeroSection = () => {
   return (
     <div className="hero-container" id="home">
-      <img className="splash-img" src="/splash.svg" alt="" />
+      <div className="container">
+        <img className="splash-img" src="/splash.svg" alt="" />
+      </div>
+      {/* <img className="splash-img" src="/splash.svg" alt="" /> */}
       <a
         href="https://816rehabandperformance.janeapp.com/#/staff_member/1/bio"
         target="_blank"
